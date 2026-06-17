d80f521766807564b2c8d5b5daec4c963700e0e6
Meyhieng <sanmeyhieng22@gmail.com>
2026-06-18 03:02:48 +0700

    fix error

--- src/main/java/com/example/idcard/IdcardApplication.java
+++ src/main/java/com/example/idcard/IdcardApplication.java
@@ -2,7 +2,6 @@ package com.example.idcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-THIS WILL BREAK THE BUILD!!!
 @SpringBootApplication
 public class IdcardApplication {
     public static void main(String[] args) {
