31692fd9d7112b467c03af5d539ad7d5c65d6083
Meyhieng <sanmeyhieng22@gmail.com>
2026-06-18 02:59:45 +0700

    test notification

--- src/main/java/com/example/idcard/IdcardApplication.java
+++ src/main/java/com/example/idcard/IdcardApplication.java
@@ -2,7 +2,7 @@ package com.example.idcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+THIS WILL BREAK THE BUILD!!!
 @SpringBootApplication
 public class IdcardApplication {
     public static void main(String[] args) {
